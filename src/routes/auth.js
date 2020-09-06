668339dedb30a3d58116a056754033af4443ae25
GHOST <ghostdevbusiness@gmail.com>
2020-09-06 02:17:19 +0100

    Placeholder

---
+++ src/routes/auth.js
@@ -0,0 +1,10 @@
+const { Router } = require('express');
+const router = Router();
+const passport = require('passport');
+
+module.exports = (connection) => {
+
+    // do auth
+    
+    return router;
+};
