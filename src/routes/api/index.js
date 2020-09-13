d272f95a18e9e9db75565ce6d14ede9e99247bb8
GHOST <ghostdevbusiness@gmail.com>
2020-09-13 14:47:21 +0100

    Fixed /api/links

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -1,4 +1,4 @@
 module.exports = (server, connection) => {
     server.use('/api/themes', require('./theme.js')());
-    server.use('/api/links', require('./theme.js')(connection));
+    server.use('/api/links', require('./links.js')(connection));
 };
