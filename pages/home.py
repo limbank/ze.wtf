aaac545e2fdc4c2ea930ecf87d77071c002f0a79
raccoondays <43695821+raccoondays@users.noreply.github.com>
2025-03-01 20:03:42 +0200

    Added alias checking

--- pages/home.py
+++ pages/home.py
@@ -3,12 +3,49 @@ from dotenv import load_dotenv
 import validators
 import random
 import string
+import json
 from utils.cookies import check_cookie, user_from_cookie
 
 # Only import urls here instead
 from models import *
 
 home = Blueprint('home', __name__, template_folder='templates')
+
+def create_link(user_id):
+    # Create URL
+    url = request.form.get('url')
+    url_name = request.form.get('name')
+    error = None
+
+    gen_msg = "Not a valid URL! Remember to include the schema."
+
+    if url is None:
+        # Form submitted without URL
+        return dict(error=gen_msg, url_available=None)
+
+    if not validators.url(url):
+        # URL is invalid
+        return dict(error=gen_msg, url_available=None)
+
+    # Validate alias if available
+    if url_name == "":
+        url_name = ''.join(random.choices(string.ascii_lowercase + string.digits, k=6))
+    else:
+        # Check if url alias is allowed
+        with open('utils/usernames.json') as f:
+            data = json.load(f)
+            if url_name in data['usernames']:
+                return dict(error="URL alias not allowed.", url_available=None)
+
+        # Verify alias not existing
+        alias_count = Link.select().where(Link.ref == url_name).count()
+        if alias_count > 0:
+            return dict(error="URL alias already exists.", url_available=None)
+
+    # Add URL to database
+    Link.create(url=url, ref=url_name, owner=user_id)
+
+    return dict(url_available=url_name, error=None)
 
 @home.route("/", methods=['GET', 'POST'], defaults={'path': ''})
 def index(path):
@@ -22,29 +59,11 @@ def index(path):
         username = current_user['username']
         user_id = current_user['user_id']
 
-    url = request.form.get('url')
-    url_name = request.form.get('name')
-    error = None
+    new_url = dict(error=None, url_available=None)
+    if request.method == 'POST':
+        new_url = create_link(user_id)
 
-    if url_name == "":
-        url_name = ''.join(random.choices(string.ascii_lowercase + string.digits, k=6))
-
-    # extractor = URLExtract()
-    # urls = extractor.find_urls(url)
-    # print(len(urls))
-
-    url_available = None
-
-    if url is not None:
-        print("URL NOT NONE")
-        print(url)
-        if not validators.url(url):
-            error = "\"" + url + "\" is not a valid URL! Remember to include the schema."
-        else:
-            Link.create(url=url, ref=url_name, owner=user_id)
-            url_available = url_name
-
-    return render_template("home.html", error=error, url_available = url_available, domain=request.host, username=username)
+    return render_template("home.html", error=new_url['error'], url_available = new_url['url_available'], domain=request.host, username=username)
 
 @home.route("/<string:path>")
 @home.route('/<path:path>')
