dcd97bc79f2e96602326165baddefe3065334ea2
Michae1Weiss <misha19989@icloud.com>
2025-03-24 19:37:40 +0100

    Finish test class scaffold, create first test

--- tests/README.md
+++ tests/README.md
@@ -1,3 +1,8 @@
+### How to run tests
+
+```commandline
+python -m unittest
+```
 
 ## Links
 [**peewee.com** - Testing Peewee Applications](https://docs.peewee-orm.com/en/latest/peewee/database.html#testing-peewee-applications)
--- tests/test_models.py
+++ tests/test_models.py
@@ -6,13 +6,24 @@ from peewee import PostgresqlDatabase
 from psycopg2 import sql
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 
-MODELS = []
+from models import Link, Invite, File, User, Cookie, Role, Permission, RolePerm, Space, Key
+
+MODELS = [Link, Invite, File, User, Cookie, Role, Permission, RolePerm, Space, Key]
 
 # use an in-memory SQLite for tests.
 test_db = PostgresqlDatabase(None)
 
-class BaseTestCase(unittest.TestCase):
 
+class BaseTestCase(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        """Creates the test database before running tests."""
+        cls.create_test_database()
+
+    @classmethod
+    def tearDownClass(cls):
+        """Drops the test database after tests are complete."""
+        cls.drop_test_database()
 
     def setUp(self):
         self.create_test_database()
@@ -20,11 +31,19 @@ class BaseTestCase(unittest.TestCase):
         # all models, we do not need to recursively bind dependencies.
         test_db.bind(MODELS, bind_refs=False, bind_backrefs=False)
 
+        test_db.init(
+            'postgres',
+            host=os.getenv('DB_HOST'),
+            port=os.getenv('DB_PORT'),
+            user=os.getenv('DB_USER'),
+            password=os.getenv('DB_PASS'),
+        )
+
         test_db.connect()
         test_db.create_tables(MODELS)
 
     def tearDown(self):
-        # Not strictly necessary since SQLite in-memory databases only live
+        # Not strictly necessary since postgres test db only live
         # for the duration of the connection, and in the next step we close
         # the connection...but a good practice all the same.
         test_db.drop_tables(MODELS)
@@ -32,10 +51,20 @@ class BaseTestCase(unittest.TestCase):
         # Close connection to db.
         test_db.close()
 
-        self.drop_test_database()
+    def test_empty_db(self):
+        # Arrange
+        link_count = Link.select().count()
+        invite_count = Invite.select().count()
 
-    def create_test_database(self):
+        # Assert
+        self.assertEqual(link_count, 0, f"Expected 0 links in the database, but found {link_count}.")
+        self.assertEqual(invite_count, 0, f"Expected 0 invites in the database, but found {invite_count}.")
+
+    @staticmethod
+    def create_test_database():
         """
+        Creates the test database if it doesn't exist.
+
         Refs:
             1. https://www.geeksforgeeks.org/python-postgresql-create-database/
             2. https://stackoverflow.com/questions/34484066/create-a-postgres-database-using-python
@@ -57,16 +86,20 @@ class BaseTestCase(unittest.TestCase):
 
         # Use the psycopg2.sql module instead of string concatenation
         # in order to avoid sql injection attacks.
-        cur.execute(sql.SQL("CREATE DATABASE {}").format(
-            sql.Identifier(test_db_name))
+        cur.execute(sql.SQL(
+            "SELECT 'CREATE DATABASE {db_name}' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = '{db_name}')").format(
+            db_name=sql.Identifier(test_db_name))
         )
 
         # Closing the connection
         cur.close()
         con.close()
 
-    def drop_test_database(self):
+    @staticmethod
+    def drop_test_database():
         """
+        Drops the test database after tests are complete.
+
         Refs:
             1. https://www.geeksforgeeks.org/python-postgresql-create-database/
             2. https://stackoverflow.com/questions/34484066/create-a-postgres-database-using-python
@@ -88,7 +121,7 @@ class BaseTestCase(unittest.TestCase):
 
         # Use the psycopg2.sql module instead of string concatenation
         # in order to avoid sql injection attacks.
-        cur.execute(sql.SQL("DROP DATABASE {}").format(
+        cur.execute(sql.SQL("DROP DATABASE IF EXISTS {}").format(
             sql.Identifier(test_db_name))
         )
 
