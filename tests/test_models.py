977d1648a82123e8e9485406c66e993b8cbc9380
Michae1Weiss <misha19989@icloud.com>
2025-03-24 19:49:16 +0100

    Fix create_test_database test database creation, finish test_empty_db test

--- tests/test_models.py
+++ tests/test_models.py
@@ -10,15 +10,23 @@ from models import Link, Invite, File, User, Cookie, Role, Permission, RolePerm,
 
 MODELS = [Link, Invite, File, User, Cookie, Role, Permission, RolePerm, Space, Key]
 
-# use an in-memory SQLite for tests.
 test_db = PostgresqlDatabase(None)
-
 
 class BaseTestCase(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        """Creates the test database before running tests."""
+        """Creates und initializes the test database before running tests."""
         cls.create_test_database()
+
+        test_db_name = (os.getenv('DB_NAME') or 'sqlite') + '_test'
+
+        test_db.init(
+            test_db_name,
+            host=os.getenv('DB_HOST'),
+            port=os.getenv('DB_PORT'),
+            user=os.getenv('DB_USER'),
+            password=os.getenv('DB_PASS'),
+        )
 
     @classmethod
     def tearDownClass(cls):
@@ -30,14 +38,6 @@ class BaseTestCase(unittest.TestCase):
         # Bind model classes to test db. Since we have a complete list of
         # all models, we do not need to recursively bind dependencies.
         test_db.bind(MODELS, bind_refs=False, bind_backrefs=False)
-
-        test_db.init(
-            'postgres',
-            host=os.getenv('DB_HOST'),
-            port=os.getenv('DB_PORT'),
-            user=os.getenv('DB_USER'),
-            password=os.getenv('DB_PASS'),
-        )
 
         test_db.connect()
         test_db.create_tables(MODELS)
@@ -52,13 +52,31 @@ class BaseTestCase(unittest.TestCase):
         test_db.close()
 
     def test_empty_db(self):
+        # Note: yes we can simplify this test and use a for loop, but I don't care lol
+
         # Arrange
         link_count = Link.select().count()
         invite_count = Invite.select().count()
+        file_count = File.select().count()
+        user_count = User.select().count()
+        cookie_count = Cookie.select().count()
+        role_count = Role.select().count()
+        permission_count = Permission.select().count()
+        role_perm_count = RolePerm.select().count()
+        space_count = Space.select().count()
+        key_count = Key.select().count()
 
         # Assert
         self.assertEqual(link_count, 0, f"Expected 0 links in the database, but found {link_count}.")
         self.assertEqual(invite_count, 0, f"Expected 0 invites in the database, but found {invite_count}.")
+        self.assertEqual(file_count, 0, f"Expected 0 files in the database, but found {file_count}.")
+        self.assertEqual(user_count, 0, f"Expected 0 users in the database, but found {user_count}.")
+        self.assertEqual(cookie_count, 0, f"Expected 0 cookies in the database, but found {cookie_count}.")
+        self.assertEqual(role_count, 0, f"Expected 0 roles in the database, but found {role_count}.")
+        self.assertEqual(permission_count, 0, f"Expected 0 permissions in the database, but found {permission_count}.")
+        self.assertEqual(role_perm_count, 0, f"Expected 0 role perms in the database, but found {role_perm_count}.")
+        self.assertEqual(space_count, 0, f"Expected 0 spaces in the database, but found {space_count}.")
+        self.assertEqual(key_count, 0, f"Expected 0 keys in the database, but found {key_count}.")
 
     @staticmethod
     def create_test_database():
@@ -84,12 +102,13 @@ class BaseTestCase(unittest.TestCase):
 
         test_db_name = (os.getenv('DB_NAME') or 'sqlite') + '_test'
 
-        # Use the psycopg2.sql module instead of string concatenation
-        # in order to avoid sql injection attacks.
-        cur.execute(sql.SQL(
-            "SELECT 'CREATE DATABASE {db_name}' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = '{db_name}')").format(
-            db_name=sql.Identifier(test_db_name))
+        cur.execute(
+            sql.SQL("SELECT 1 FROM pg_database WHERE datname = {}").format(sql.Literal(test_db_name))
         )
+        exists = cur.fetchone()
+
+        if not exists:
+            cur.execute(sql.SQL("CREATE DATABASE {}").format(sql.Identifier(test_db_name)))
 
         # Closing the connection
         cur.close()
