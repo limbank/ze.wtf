7f62424830f36a9dcf77c8662e4bceafd80096f2
raccoondays <43695821+raccoondays@users.noreply.github.com>
2025-03-27 01:00:03 +0200

    Fixed error page

---
+++ changelog/small_fixes.md
@@ -0,0 +1,11 @@
+title: Small fixes
+date: 03/25/2025
+
+Just pushing a couple small fixes and changes while I prepare to refactor more and implement other larger features.
+
+Latest changes:
+
+- Changed the size of the captcha on both Login and Register pages
+- Changed the captcha font to Inconsolata
+- Small CSS fixes here and there
+- Reorganized some of the routes and pages
--- pages/error.py
+++ pages/error.py
@@ -1,28 +1,24 @@
 from flask import Blueprint, render_template
 from dotenv import load_dotenv
-from utils.cookies import check_cookie, user_from_cookie
+from utils.auth import authenticate
 from werkzeug.exceptions import HTTPException
 
 error = Blueprint('error', __name__, template_folder='templates')
 
-# @error.app_errorhandler(Exception)
-# def handle_error(e):
-#     # Get token for the header
-#     valid_cookie = check_cookie()
-#     username = None
-#     user_id = None
+@error.app_errorhandler(Exception)
+@authenticate
+def handle_error(e):
+    username = None
 
-#     if valid_cookie != False:
-#         current_user = user_from_cookie(valid_cookie)
-#         username = current_user['username']
-#         user_id = current_user['user_id']
+    if  g.current_user is not None:
+        username = g.current_user['username']
 
-#     # Doesn't work for errors on spaces due to the domain being different...
+    # Doesn't work for errors on spaces due to the domain being different...
 
-#     code = 500
-#     if isinstance(e, HTTPException):
-#         code = e.code
+    code = 500
+    if isinstance(e, HTTPException):
+        code = e.code
 
-#     error_data = dict(code = code, text = str(e))
+    error_data = dict(code = code, text = str(e))
 
-#     return render_template('error.html', error=error_data, username=username), code
+    return render_template('error.html', error=error_data, username=username), code
